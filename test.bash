5d6925b86f0246565181c25ff73b4e917817d9b8
Ofer Kessler <ofer.kessler@arm.com>
2019-07-18 16:54:48 +0300

    step 1

---
+++ test.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+id
+
+cd /home/kessler/paas-labs/ofer-cd
+
+export KUBECONFIG=$PWD/kubeconfig
+
+echo "Cluster Info/n"
+kubectl cluster-info
+
+echo "Pod list for namespace gocd/n"
+splatt kubectl -n gocd get all
+
+echo "Helm list"
+helm list
+
+exit
