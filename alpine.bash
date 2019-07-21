bce6a039a3310e23b907ae0ae37c34ebc8bbc227
Ofer Kessler <ofer.kessler@arm.com>
2019-07-21 16:38:51 +0300

    t

--- alpine.bash
+++ alpine.bash
@@ -14,13 +14,27 @@ source /home/kessler/v0.19.2/bin/activate
 #echo "Helm list"
 #helm list
 
-echo $CHART_NAME
+# echo $CHART_NAME
 
 if [ -z $CHART_NAME ]; then
 	echo "no chart name"
-else
-	env
-	splatt apply chart ${HOME}/pipelines/${GO_PIPELINE_NAME}/${CHART_NAME}
+	exit 1
 fi
 
-
+case "$1" in
+'apply')
+echo "Applying chart"
+splatt apply chart ${HOME}/pipelines/${GO_PIPELINE_NAME}/${CHART_NAME}
+;;
+'status')
+echo "Check Chart status "
+splatt helm status  ${HOME}/pipelines/${GO_PIPELINE_NAME}/${CHART_NAME}
+;;
+'render')
+echo "Render Chart  "
+splatt apply chart ${HOME}/pipelines/${GO_PIPELINE_NAME}/${CHART_NAME} -r
+;;
+'help')
+echo "Usage: $0 [apply|status]"
+;;
+esac
