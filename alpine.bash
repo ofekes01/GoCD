4804345f0475673baede272caa0fa2489c430e60
Ofer Kessler <ofer.kessler@arm.com>
2019-07-21 11:53:20 +0300

    ofer-3

--- alpine.bash
+++ alpine.bash
@@ -4,12 +4,22 @@ cd /home/kessler/paas-labs/ofer-cd
 
 export KUBECONFIG=$PWD/kubeconfig
 
-echo "Cluster Info/n"
-kubectl cluster-info
+#echo "Cluster Info/n"
+#kubectl cluster-info
 
-echo "Pod list for namespace gocd/n"
+#echo "Pod list for namespace gocd/n"
 source /home/kessler/v0.19.2/bin/activate
-splatt kubectl -n gocd get all
+#splatt kubectl -n gocd get all
 
-echo "Helm list"
-helm list
+#echo "Helm list"
+#helm list
+
+echo $CHART_NAME
+
+if [ -z $CHART_NAME ]; then
+	echo "no chart name"
+else
+	splatt apply chart $CHART_NAME
+fi
+
+
